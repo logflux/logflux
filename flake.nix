89242ccb005cbdef86da93fdbcb1e47d6115cfef
Robert Ricci <ricci@cs.utah.edu>
2024-02-09 12:06:51 -0700

    Add a rudimentary nix flake
    Right now, it only provides a devshell and only on x86 - still need to
    build an actual package.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake for the logflux suite of auomatic log parsers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = [
+        (nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (python-pkgs: [
+          python-pkgs.pandas
+          python-pkgs.numpy
+          python-pkgs.scikit-learn
+          python-pkgs.torch
+        ]))
+      ];
+      PYTHONPATH = self + "/src";
+    };
+  };
+}
+
